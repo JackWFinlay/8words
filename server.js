c6c0c056dca35a84d1e4829a5e91b266ad99c055
Jack Witbrock Finlay <jack.witbrock@gmail.com>
2015-11-30 21:31:00 +1300

    Updates 30/11/2015

--- server.js
+++ server.js
@@ -40,12 +40,12 @@ router.use(function(req, res, next) {
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });   
+	res.json({ message: 'Nothing here!' });
 });
 
 // more routes for our API will happen here
 
-// on routes that end in /sentence
+// on routes that end in /sentences
 router.route('/sentences')
 	
 	// create a sentence
